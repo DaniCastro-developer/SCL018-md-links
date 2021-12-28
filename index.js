70c5cad105c98e140008cff4fbc16b67d8e4982e
Daniela Castro Salazar <88462620+DaniCastro-developer@users.noreply.github.com>
2021-12-28 18:39:02 -0300

    listo javascript API

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
 import chalk from 'chalk';
-import { readFileData, validate } from './module-read.js'; 
+/* import path from 'path'; */
+import {mdLinks} from './module-read.js';
 
-const arg = 'holanda.md';
+
+/* const arg = 'hola.md'; */
+
+// Prueba con CLI
+/* const pathFile = process.argv[2];
+const validateOption = process.argv[3]; */
+
+// Prueba con  Javascript API
+
+const pathFile = 'hola.md';
+const validateOption = ' ';
 
 
-    readFileData(arg)
-    .then(links=>{
-        validate(links)
-        .then(res => {
-            res.map(link => {
-                if (link.response === "OK") {
-                  console.log(chalk.yellow(link.type)+ " " + chalk.magenta(link.href)+" "+chalk.bgGreen(link.status)+" "+chalk.bgGreen(link.response));
-                } else if (link.response === "fail"){
-                  console.log(chalk.yellow(link.type)+ " " + chalk.magenta(link.href)+" "+chalk.bgRed(link.status)+" "+chalk.bgRed(link.response));
-            }
-              })
-        })
+const options = {
+  validate: false,
+};
+
+if (validateOption === '--validate') {
+  options.validate = true;
+} else if (validateOption !== '--validate' ) {
+  options.validate = false;
+}
+
+
+mdLinks(pathFile, [validateOption])
+    .then(links => {
+      if (options.validate === false) {
+        links.map(link => {
+          console.log(chalk.green(link.type) + ' ' + chalk.yellow(link.href) + ' ' + chalk.blue(link.text));
+        });
+      } else if (options.validate === true) {
+        links.map(link => {
+          if (link.response === 'OK') {
+            console.log(chalk.yellow(link.type)+' '+ chalk.magenta(link.href)+" "+chalk.bgGreen(link.status)+" "+chalk.bgGreen(link.response));
+          } else if (link.response === 'fail') {
+            console.log(chalk.yellow(link.type)+' '+ chalk.magenta(link.href)+' '+chalk.bgRed(link.status)+ ' ' +chalk.bgRed(link.response));
+          }
+        });
+      }
     })
-
-        .catch(err=> {
-            console.log(chalk.magenta(err))});
+    .catch(err => {
+      console.log(chalk.magenta(err));
+    });
--- module-read.js
+++ module-read.js
@@ -3,58 +3,76 @@ import path from 'path';
 import fetch from 'node-fetch';
 
 /* const path = require('path'); */
-import  markdowndLinkExtractor from 'markdown-link-extractor';
-
-
+import markdowndLinkExtractor from 'markdown-link-extractor';
 
 export const readFileData = (arg) => {
   return new Promise((resolve, reject) => {
-    
-      fs.readFile(arg, 'utf8', (err, data) => { 
-        const ext = path.extname(arg);//leer ruta del documento y trabajarla 
-        let links = markdowndLinkExtractor(data,true);
-        const linksArray = [];
-          links.forEach((details)=>{ 
-              /* console.table({details}) */
-              linksArray.push(details);
+    fs.readFile(arg, 'utf8', (err, data) => {
+      const ext = path.extname(arg);// leer ruta del documento y trabajarla
+      let links = markdowndLinkExtractor(data, true);
+      const linksArray = [];
+      links.forEach((details) => {
+        /* console.table({details}) */
+        linksArray.push(details);
 
-              if (ext === '.md') {         
-                resolve(linksArray); // promesa que pasa a estar resuelta y muestra el archivo con extensión .md
-       } else if (ext !== '.md') {
-                reject('this file is not .md' + err); //Promesa que marca el error del documento
-       }
-          });   
-        
-    })
+        if (ext === '.md') {
+          resolve(linksArray);
+        } else if (ext !== '.md') {
+          reject('this file is not .md' + (err));
+        }
+      });
+    });
   })
-
-// .then(file => console.log(file))
-    .catch(err => console.log('Ingresa un archivo con extension .md', err));
-      }
+      // .then(file => console.log(file))
+      .catch(err => console.log('Ingresa un archivo con extension .md', err));
+};
 
 export const validate = (links) => {
   return Promise.all(links.map(link => {
     return new Promise((resolve, reject) => {
       fetch(link.href)
-      .then(res => {
-        if (res.status > 400) {
-          link.status = res.status;
-          link.response = "fail";
-          resolve(link);
-        } else {
-          link.status = res.status;
-          link.response = res.statusText;
-          resolve(link); 
-        }
-      })
-      .catch(err => {
-          link.status = null;
-          link.response = "fail"
-          resolve(link);
-      });
+          .then(res => {
+            if (res.status > 400) {
+              link.status = res.status;
+              link.response = 'fail';
+              resolve(link);
+            } else {
+              link.status = res.status;
+              link.response = res.statusText;
+              resolve(link);
+            }
+          })
+          .catch(err => {
+            link.status = null;
+            link.response = 'fail';
+            resolve(link);
+          });
     });
   }));
 };
 
 
+export const mdLinks = (path, option) => {
+  return new Promise((resolve, reject) => {
+    if (option[0] !== '--validate') {
+      readFileData(path)
+          .then(links => {
+            resolve(links);
+          })
+          .catch(err => {
+            reject(err);
+          });
+    } else if (option[0] === '--validate') {
+      readFileData(path)
+          .then(links => {
+            validate(links)
+                .then(res => {
+                  resolve(res);
+                });
+          });
+    } else {
+      reject('¿Qué deseas hacer? Intenta con:\n --validate\n'(err));
+    };
+  });
+};
 
